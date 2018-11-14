0a69caaf6836f5e6b5bde150a0ae522a9385763c
ndnavar <44449934+ndnavar@users.noreply.github.com>
2018-11-14 16:26:11 -0500

    Update track.php

--- Controller/track.php
+++ Controller/track.php
@@ -1,6 +1,7 @@
 <?php
 require('../View/common.php');
 if($_POST && !empty($_POST['fname'])){	
+	//Nos permite revisar los datos en los cuales ha estdo registrado 
 	$result = $db->track_qr(explode("?", $_POST['codigo'],2)[0]);
 	echo json_encode($result);
 }
