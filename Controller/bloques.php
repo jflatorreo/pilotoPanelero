5a89542935593e6a8e67c350e6151842442b219c
ndnavar <44449934+ndnavar@users.noreply.github.com>
2018-11-14 16:11:01 -0500

    Update bloques.php

--- Controller/bloques.php
+++ Controller/bloques.php
@@ -1,6 +1,7 @@
 <?php require('../View/common.php');
 
 if(isset($_POST) && !empty($_POST['id_curso']) ){
+	// Se busca el elemento en l base de datos.
 	$data = $db->get_bloques($_POST['id_curso']);
 }
 // JSON
